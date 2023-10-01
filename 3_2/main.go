e368e6ad8963b889f03e4c1873d5d3f277e80f50
Keiji Miyazaki <miyah.dev@gmail.com>
2023-10-01 20:18:16 +0900

    [3-2] io.ReadFull

--- 3_2/main.go
+++ 3_2/main.go
@@ -8,13 +8,26 @@ import (
 )
 
 func main() {
-	fmt.Println("With io.ReadAll")
-	r := strings.NewReader("Go is general-purpose language designed with systems programming in mind.")
+	fmt.Println("----- With io.ReadAll -----")
+	r1 := strings.NewReader("Go is general-purpose language designed with systems programming in mind.")
 
-	buffer, err := io.ReadAll(r)
-	if err != nil {
-		log.Fatal(err)
+	buf1, err1 := io.ReadAll(r1)
+	if err1 != nil {
+		log.Fatal(err1)
 	}
 
-	fmt.Printf("%s\n", buffer)
+	fmt.Printf("%s\n", buf1)
+
+	fmt.Println("----- With io.ReadFull -----")
+
+	r2 := strings.NewReader("Go is general-purpose language designed with systems programming in mind.")
+
+	buf2 := make([]byte, 5)
+	size2, err2 := io.ReadFull(r2, buf2)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Printf("Size: %d, Content: %s\n", size2, buf2)
 }
